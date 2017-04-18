4aa6d3e2cfcdb2ad52b5339284c828252977964b
Jonathan <jonathan.c.strom@gmail.com>
2017-04-18 14:48:00 +0200

    Fixed findMax function.

--- laboration2/main.s
+++ laboration2/main.s
@@ -1,19 +1,21 @@
 .data
 
 test:
-.word 1
-.word 3
-.word 5
-.word 7
-.word 9
-.word 8
-.word 6
-.word 4
-.word 2
-.word 0
+	.word 1
+	.word 3
+	.word 5
+	.word 7
+	.word 9
+	.word 8
+	.word 6
+	.word 4
+	.word 2
+	.word 0
+
 textA: .asciz "Lab1 , Assignment  2\n"
 textB: .asciz "The max is %d\n"
 textC: .asciz "Done\n"
+
 .text
 .global  main
 .extern  printf
@@ -25,16 +27,17 @@ findMax:
 PUSH     {lr}
 
 	MOV r3, #0
-
-
+	/*LDR r0, r0
+	BL	printf
+*/
 again:
-	/* Check if it's 0 */
+	/* Check if it's 0, jump to finish if it is */
 	LDR r2, [r0]
 	CMP r2, #0
 	BEQ finish
 
 	/* Check value if it's less than... */
-	CMPS r2, r3
+	CMP r2, r3
 	BLT jump
 
 	/* Set the new max value */
@@ -49,7 +52,7 @@ jump:
 
 finish:
 	/* Store max value and exit function */
-	STR r3, [r0]
+	MOV r0, r3
 
 
 POP	{pc}
@@ -64,16 +67,16 @@ main  function
 **********************/
 main:
 PUSH     {lr}
-LDR      r0 , =textA
-BL        printf
-LDR      r0 , =test
-BL        findMax
-MOV      r1 , r0
-LDR      r0 , =textB
-BL        printf
-LDR      r0 , =textC
-BL        printf
-
+	LDR      r0 , =textA
+	BL        printf
+	LDR      r0 , =test
+	BL        findMax
+	MOV      r1 , r0
+	LDR      r0 , =textB
+	BL        printf
+	LDR      r0 , =textC
+	BL        printf
+	MOV	r0, #0
 POP	{pc}
 
 .end
