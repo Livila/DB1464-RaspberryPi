418bad1656cd097a9ef1a47c65c29ae332d1ac46
Jonathan <jonathan.c.strom@gmail.com>
2017-04-20 15:52:39 +0200

    First release, almost working copy.

--- laboration3/main.s
+++ laboration3/main.s
@@ -8,15 +8,43 @@ printText: .asciz "Factorial: %d\n"
 .global main
 .extern printf
 
+
+factorial:
+
+PUSH	{lr}
+/*
+again:
+	MOV r1, #1
+
+	LDR r2, [r0]
+
+	CMP r2, #0
+	BEQ finish
+	STR r2, [r0]
+	BL factorial
+
+finish:
+	MUL r2, r1
+	MOV r0, r2
+*/
+
+	MOV r0, #3
+
+POP	{pc}
+
 main:
 PUSH	{lr}
 
+	MOV	r0, #4
+	BL	factorial
+	MOV	r1, r0
 	LDR	r0, =printText
 	BL	printf
 
 
 
-
+	/* Return 0 at the end of the program. */
+	MOV r0, #0
 
 POP	{pc}
 .end
