e546a7f853f4455be795e0f6b1521ded811d0325
Jonathan Ström <jonathan.c.strom@gmail.com>
2017-08-11 12:41:37 +0200

    Fixed and formatted the test files.

--- laboration3/testFiles/runscriptTest.sh
+++ laboration3/testFiles/runscriptTest.sh
@@ -1,5 +1,22 @@
-echo 'Compiling testProgrmas...'
+echo 'Compiling...';
+echo;
+
+# Compile the files.
 gcc -g -o testProgram testProgram.s
-echo 'Completed compiling. Executing the file...'
-./testProgram
-echo ''
+if [ $? -eq 0 ]; then
+    # No error.
+    echo 'Compiling completed. Executing the file...';
+    echo 'Result:';
+    echo;
+
+    # Run the executable file.
+    ./testProgram
+
+    # Remove the executable file.
+    rm testProgram
+else
+    # Some error occured...
+    echo 'Some error occured, could not compile...';
+fi
+
+echo;
