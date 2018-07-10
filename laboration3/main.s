6dac737490330495b57d61348b80636743f6cb0b
FeKozma <felix.kozma@gmail.com>
2018-07-10 11:45:04 +0200

    added comments

--- laboration3/main.s
+++ laboration3/main.s
@@ -25,13 +25,13 @@ MAXPOS:		.quad	63
 
 inImage:
 	#	//movabsq = flytta imidiet värde
-	movq $bufIn, %rdi			# The stack is set to 16 bytes aligned.
-	movabsq $65, %rsi			# Add buf to arg1.
-	movq stdin, %rdx			# Read 64-1 symbols, arg2.
-								# 	64 bytes chars.
-								# 	1 byte \0 to end the buff stream.
-	movq $0, bufInPos			# Read from console, arg3.
-	call fgets					# Read input from user.
+	movq $bufIn, %rdi	# The stack is set to 16 bytes aligned.
+	movabsq $65, %rsi	# Add buf to arg1.
+	movq stdin, %rdx	# Read 64-1 symbols, arg2.
+					# 	64 bytes chars.
+					# 	1 byte \0 to end the buff stream.
+	movq $0, bufInPos	# Read from console, arg3.
+	call fgets		# Read input from user.
 
 ret
 
@@ -40,47 +40,47 @@ getInt:
 	#return val
 	mov $0, %rax
 
-	leaq bufIn, %r8 # load buf
-	movq bufInPos, %r9 # get bufinPos
-	movq bufInPos, %r12 # get bufinPos
+	leaq bufIn, %r8		# load buf
+	movq bufInPos, %r9 	# get bufinPos
+	movq bufInPos, %r12 	# get bufinPos
 
-	addq %r9, %r8
+	addq %r9, %r8		#load the first character
 
-	movq $1, %r10 # positive
+	movq $1, %r10		#assume it is positive (the end value will be multiplied with this value)
 
-#check if it is  space before
+#check if it is  space before character
 checkIfSpace:
-	cmpb $' ', (%r8)
-	jne endCheckIfSpace
-	incq %r8 # get next char
-	incq %r12 # increase bufInPos
-	jmp checkIfSpace
+	cmpb $' ', (%r8) 	# is the current character a space
+	jne endCheckIfSpace  	# the current character is not a space
+	incq %r8		# get next char
+	incq %r12		# increase bufInPos
+	jmp checkIfSpace	# check if next character is a space
 endCheckIfSpace:
 
-	cmpb $45, (%r8, 1) # check if char are -
-	jne cont
-	movq $-1, %r10 # negative
+	cmpb $45, (%r8, 1)	# check if char are '-'
+	jne cont 		# character is not a '-' signe and therefore positive
+	movq $-1, %r10 		# the value is negative
 
 cont:
-	cmpb	 $43, (%r8, 1) # check if char is positive
-	jne cont1
-	incq %r8 # skip the + sign
-	incq %r12
+	cmpb	 $43, (%r8, 1)	# check if char is '+'
+	jne cont1		# jump if the char is not '+'
+	incq %r8 		# skip the + sign
+	incq %r12 		#increase buf in pos
 
 
 cont1:
-	cmpq $-1, %r10
+	cmpq $-1, %r10 		#check if the character '-' was found
 	jne checkIfAtEndAndCallInImage
-	incq %r8 # skip the - sign
-	incq %r12  
+	incq %r8 		# skip the - sign  (this is made now not to skipp the '+' character)
+	incq %r12  		#increase buf in pos
 
 #check if the string is to short
 checkIfAtEndAndCallInImage:
-#check if r8 = \n
+#check if r8 = '\n'
 	cmpq $10, (%r8, 1)
 	jne luuuup
 
-	call inImage
+	call inImage		#if we are at end of the string demand new characters to be entered
 	jmp getInt
 	
 # check if it is at the end of the buffert
@@ -98,20 +98,20 @@ luup:
 
 	
 	# Check if it's a number.
-	# is the value between ascii 0 and 9
+	# is the first characters value is between ascii 0 and 9
 	cmpb $'0', (%r8)
-	jl finluup
+	jl finluup 		#jump if lower than '0'
 	cmpb $'9', (%r8)
-	jg finluup
+	jg finluup		#jump if higher than '9'
 
 	movzbq (%r8), %r9
 	# save number
-	subq $48, %r9 # convert from ascii to int
-	imulq $10, %rax # make space for new number in returnvalue
-	add %r9, %rax # add to return value
+	subq $48, %r9 		# convert from ascii to int
+	imulq $10, %rax 	# make space for new number in returnvalue
+	add %r9, %rax		# add to return value
 
-	incq %r8 # get next char
-	incq %r12 # increase bufInPos
+	incq %r8		# get next char
+	incq %r12		# increase bufInPos
 	jmp luup
 finluup:
 
@@ -234,36 +234,27 @@ loopPutInt:
 endPutInt:
 
 #if it was minus input a '-' character
-cmpq $-1, %r11
-jne notMinusSigne
-movb $'-', bufOut(, %rbx, 1)
-incq %rbx
+	cmpq $-1, %r11
+	jne notMinusSigne
+	movb $'-', bufOut(, %rbx, 1)
+	incq %rbx
 notMinusSigne:
 
-#pop the latest char
-pop %r9
 
-#convert to ascii
-addb $48, %r9b
-# add char to buf
-movb %r9b, bufOut(, %rbx, 1)
-#increase bufOutPos
-incq %rbx
-#decrease r8
-decq %r8
-#check if r8 == 0
-cmpq $0, %r8
-jne endPutInt
+	pop %r9			#pop the latest char
 
-#null terminated
-#movb $0, %r9b
-# add char to buf
-#movb %r9b, bufOut(, %rbx, 1)
+	addb $48, %r9b		#convert to ascii
+	movb %r9b, bufOut(, %rbx, 1) # add char to buf
+	incq %rbx		#increase bufOutPos
+	decq %r8		#decrease amount of times to pop
 
-mov %rbx, bufOutPos
-	#movq bufOut, %r8
-	#addq bufOutPos, %r8
-	#movq %rdi, %r8
+	cmpq $0, %r8		#check if r8 == 0
+	jne endPutInt		#continue pop if r8 != 0
+
+
+
+	mov %rbx, bufOutPos
+
 
 
 ret
@@ -273,37 +264,34 @@ putText:
 
 
 
-# </testPrint>
-# <TestPrintBuff>  
+ 
 	movq 	bufOutPos, %rbx
 putTLoop:
 
 	cmpb 	$0, (%rdi)
-	je 	finT
-	movb 	(%rdi), %al
+	je 	finT 		#jump if at end of the text
+	movb 	(%rdi), %al 	#move one char to %al
 
-	movb 	%al, bufOut(, %rbx, 1)
+	movb 	%al, bufOut(, %rbx, 1) # save to memory
 
+	#jump to next char
 	incq 	%rdi
 	incq 	%rbx
 	jmp 	putTLoop
 
 finT:
+	#update bufOutPos
 	movq 	%rbx, bufOutPos
 
 
 
 	
 	
-# </TestPrintBuff>
-	
 
-
-# <TestPrintBuff>
 	#push %rbx
 
 	
-# </TestPrintBuff>
+
 
 # print
 
